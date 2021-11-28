55ac67ba7b2ce6668dc8322f6b5dbe86c1f78032
Bradford Sherman <brad@Bradfords-MacBook-Pro-2.local>
2021-11-28 16:57:37 -0500

    explicitly copy hidden files

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 echo $1
 echo $2
 cp -r /app/* /github/workspace/
+cp -r /app/.cljs_node_repl /github/workspace/
+cp -r /app/.clj-kondo /github/workshpace/
 echo $(ls -al)
 node /github/workspace/main.js $1 $2
